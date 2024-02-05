a8f7b0c51c25a65f67afccfe4ccfed9794a0b6f9
bullky11 <pozsmarc112@gmail.com>
2024-02-05 19:49:34 +0100

    1.0 version upload

---
+++ src/Square.js
@@ -0,0 +1,10 @@
+
+export default function Square(props) {
+  return (
+    <>
+      <button onClick={props.onSquareClick} className="square">
+        {props.value}
+      </button>
+    </>
+  );
+}
